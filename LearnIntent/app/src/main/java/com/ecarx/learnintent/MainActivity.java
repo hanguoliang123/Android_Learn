3923821f102d60dd2bbb6b6dcbe17e37c58f89da
“hanguoliang123 <1073253115@qq.com>
2018-08-21 23:30:33 +0800

    添加:隐式Intent的使用-opt-v1.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
+++ LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnStartMyAty).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent("com.ecarx.learnintent.intent.action.MyAty"));   //显式Intent,直接指明了被启动的类的定义
+                startActivity(new Intent(MyAty.ACTION));   //显式Intent,直接指明了被启动的类的定义
                                                             //隐式Intent,<intent-filter>中指定
             }
         });
