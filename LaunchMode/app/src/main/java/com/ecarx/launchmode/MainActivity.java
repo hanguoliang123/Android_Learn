04a31ab5623c4729cc86ef02cfbdf265ef170462
“hanguoliang123 <1073253115@qq.com>
2018-08-21 21:24:47 +0800

    添加:启动模式-standard mode.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

---
+++ LaunchMode/app/src/main/java/com/ecarx/launchmode/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ecarx.launchmode;
+
+import android.content.Intent;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText(String.format("TaskID:%d\nCurrent ActivityId %s",getTaskId(),toString()));
+
+        findViewById(R.id.btnStartSelf).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
