52989b9a8f884f1101e600f864af486175079ed6
“hanguoliang123 <1073253115@qq.com>
2018-08-21 22:37:56 +0800

    添加:显式Intent的使用.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

---
+++ LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ecarx.learnintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStartMyAty).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MyAty.class));   //显式Intent,直接指明了被启动的类的定义
+            }
+        });
+
+    }
+}
---
+++ LearnIntent/app/src/main/java/com/ecarx/learnintent/MyAty.java
@@ -0,0 +1,17 @@
+package com.ecarx.learnintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+;
+
+/**
+ * Created by Lenovo on 2018/8/21.
+ */
+public class MyAty extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myaty);
+    }
+}
