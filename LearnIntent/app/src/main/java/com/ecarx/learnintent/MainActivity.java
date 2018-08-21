7edf054ced8e3094662bd4bb5bd7a57e87613faf
“hanguoliang123 <1073253115@qq.com>
2018-08-21 23:35:08 +0800

    添加:隐式Intent的使用-opt-v1.1:添加注释
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
+++ LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(MyAty.ACTION));   //显式Intent,直接指明了被启动的类的定义
                                                             //隐式Intent,<intent-filter>中指定
+                                                            //隐式Intent优势:可以启动另一个应用的Activity,A应用不能获取到B应用的Activity的类的定义的
             }
         });
 
