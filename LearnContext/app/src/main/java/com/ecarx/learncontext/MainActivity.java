0179d7bd2dff7e6d20e8f8d0ad0b33a245f88af3
“hanguoliang123 <1073253115@qq.com>
2018-08-22 22:48:02 +0800

    add:Context的作用，获取ImageView资源.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- LearnContext/app/src/main/java/com/ecarx/learncontext/MainActivity.java
+++ LearnContext/app/src/main/java/com/ecarx/learncontext/MainActivity.java
@@ -2,6 +2,7 @@ package com.ecarx.learncontext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        tv = new TextView(MainActivity.this);
+//        tv = new TextView(MainActivity.this);
         //tv.setText("Hello Android.");
-        tv.setText(R.string.hello_world);
-        setContentView(tv);
+//        tv.setText(R.string.hello_world);
+//        setContentView(tv);
+
+        ImageView iv = new ImageView(this);
+        iv.setImageResource(R.mipmap.ic_launcher);
+        setContentView(iv);
 
         System.out.println(getResources().getText(R.string.hello_world));
     }
