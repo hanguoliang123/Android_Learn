9e8ee26de3dcb42d0656674b76bf2449c41b456f
“hanguoliang123 <1073253115@qq.com>
2018-08-22 21:53:16 +0800

    add:添加Intent 过滤相关选项
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
+++ LearnIntent/app/src/main/java/com/ecarx/learnintent/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ecarx.learnintent;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnStartMyAty).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MyAty.ACTION));   //显式Intent,直接指明了被启动的类的定义
+                startActivity(new Intent(MyAty.ACTION, Uri.parse("app1://helo")));   //显式Intent,直接指明了被启动的类的定义
                                                             //隐式Intent,<intent-filter>中指定
                                                             //隐式Intent优势:可以启动另一个应用的Activity,A应用不能获取到B应用的Activity的类的定义的
             }
