4dec2ab6045e5cb4e0022a47c732503ff6864448
“hanguoliang123 <1073253115@qq.com>
2018-08-30 21:47:29 +0800

    02-06_创建别的应用程序可以读写的文件.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

---
+++ CreateFile/app/src/main/java/com/ecarx/createfile/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ecarx.createfile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 创建一个文件，这个文件可以让别的应用程序读写
+        try {
+            FileOutputStream fos = openFileOutput("readable.txt", MODE_WORLD_READABLE);
+            fos.write("data".getBytes());
+            fos.close();
+
+            fos = openFileOutput("writeable.txt", MODE_WORLD_WRITEABLE);
+            fos.write("data".getBytes());
+            fos.close();
+
+            fos = openFileOutput("public.txt", MODE_WORLD_WRITEABLE|MODE_WORLD_READABLE);
+            fos.write("data".getBytes());
+            fos.close();
+
+            fos = openFileOutput("private.txt", MODE_PRIVATE);
+            fos.write("data".getBytes());
+            fos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
