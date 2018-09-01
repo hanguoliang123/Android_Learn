19f9d87475a37db720c6bf7f62ea32b3d385d607
“hanguoliang123 <1073253115@qq.com>
2018-09-01 13:25:37 +0800

    02-11_生成xml文件_2-20180901.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- SaveXmlInfo/app/src/main/java/com/ecarx/savexmlinfo/MainActivity.java
+++ SaveXmlInfo/app/src/main/java/com/ecarx/savexmlinfo/MainActivity.java
@@ -4,9 +4,12 @@ import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
+import android.util.Xml;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.xmlpull.v1.XmlSerializer;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -36,9 +39,9 @@ public class MainActivity extends BaseActivity {
      */
     public void save(View view){
         /* 3.根据控件读取用户输入的文本，放入到字符串变量中 */
-        String name = mEt_name.getText().toString().trim();
-        String age = mEt_age.getText().toString().trim();
-        String id = mEt_id.getText().toString().trim();
+        final String name = mEt_name.getText().toString().trim();
+        final String age = mEt_age.getText().toString().trim();
+        final String id = mEt_id.getText().toString().trim();
         // 4.判断是否有空的内容
         if(TextUtils.isEmpty(name)||TextUtils.isEmpty(age)||TextUtils.isEmpty(id)){
             Toast.makeText(this, "信息不能为空", Toast.LENGTH_SHORT).show();
@@ -58,17 +61,44 @@ public class MainActivity extends BaseActivity {
             performCodeWithPermission("保存学生信息到sd卡", new PermissionCallback() {
                 @Override
                 public void hasPermission() {
-
-                    File file = new File(Environment.getExternalStorageDirectory(), "info.xml");
+                    // 1.创建一个xml文件的序列化器
+                    XmlSerializer serializer = Xml.newSerializer();
+                    FileOutputStream fos = null;
                     try {
-                        FileOutputStream fos = new FileOutputStream(file);
-                        fos.write(sb.toString().getBytes());
+                        fos = new FileOutputStream(new File(Environment.getExternalStorageDirectory(),"info.xml"));
+
+                        // 2.设置文件的输出和编码方式
+                        serializer.setOutput(fos,"utf-8");
+                        // 3.写xml文件的头
+                        serializer.startDocument("utf-8",true);
+
+                        // 4.写info节点
+                        serializer.startTag(null,"info");
+
+                        // 5.写student节点
+                        serializer.startTag(null,"student");
+
+                        // 6.写属性
+                        serializer.attribute(null,"id",id);
+
+                        // 7.写name
+                        serializer.startTag(null,"name");
+                        serializer.text(name);
+                        serializer.endTag(null,"name");
+                        // 8.写age
+                        serializer.startTag(null,"age");
+                        serializer.text(age);
+                        serializer.endTag(null,"age");
+
+                        serializer.endTag(null,"student");
+                        serializer.endTag(null,"info");
+                        serializer.endDocument();
                         fos.close();
-                        Toast.makeText(MainActivity.this, "保存学生信息成功!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "保存学生信息成功", Toast.LENGTH_SHORT).show();
                     } catch (Exception e) {
                         e.printStackTrace();
-                        Toast.makeText(MainActivity.this, "保存学生信息失败!", Toast.LENGTH_SHORT).show();
                     }
+
                 }
 
                 @Override
