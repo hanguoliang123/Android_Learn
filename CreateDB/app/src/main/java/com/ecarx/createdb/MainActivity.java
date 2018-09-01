4b22105e5a7bac845a49857d96e87790a310293f
“hanguoliang123 <1073253115@qq.com>
2018-09-01 23:49:15 +0800

    03-01_创建数据库_3-20180901.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- CreateDB/app/src/main/java/com/ecarx/createdb/MainActivity.java
+++ CreateDB/app/src/main/java/com/ecarx/createdb/MainActivity.java
@@ -1,17 +1,97 @@
 package com.ecarx.createdb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ecarx.createdb.dao.ContactInfoDao;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mName;
+    private EditText mphone;
+    private ContactInfoDao mDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyDBOpenHelper helper = new MyDBOpenHelper(this);
-        helper.getWritableDatabase();
+        // 1.找到需要用到的控件
+        mName = (EditText) findViewById(R.id.et_name);
+        mphone = (EditText) findViewById(R.id.et_phone);
+        // 2.new一个dao出来
+        mDao = new ContactInfoDao(this);
+    }
 
+    /**
+     * 添加一条联系人信息
+     * @param view
+     */
+    public void add(View view){
+        String name = mName.getText().toString().trim();
+        String phone = mphone.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)||TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            mDao.add(name,phone);
+            Toast.makeText(this, "添加成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 删除一条联系人信息
+     * @param view
+     */
+    public void delete(View view){
+        String name = mName.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            mDao.delete(name);
+            Toast.makeText(this, "删除成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 添加一条联系人信息
+     * @param view
+     */
+    public void update(View view){
+        String name = mName.getText().toString().trim();
+        String phone = mphone.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)||TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            mDao.update(name,phone);
+            Toast.makeText(this, "修改成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 添加一条联系人信息
+     * @param view
+     */
+    public void query(View view){
+        String name = mName.getText().toString().trim();
+        String phone = mphone.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)||TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            String query_name;
+            query_name = mDao.query(name);
+            Toast.makeText(this,"查询结果为"+query_name, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- CreateDB/app/src/main/java/com/ecarx/createdb/dao/ContactInfoDao.java
+++ CreateDB/app/src/main/java/com/ecarx/createdb/dao/ContactInfoDao.java
@@ -1,5 +1,11 @@
 package com.ecarx.createdb.dao;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.ecarx.createdb.MyDBOpenHelper;
+
 /**
  * @version $Rev$
  * @authod:Guoliang.Han
@@ -8,4 +14,71 @@ package com.ecarx.createdb.dao;
  * @updateDes ${TODO}
  */
 public class ContactInfoDao {
+
+    private final MyDBOpenHelper helper;
+
+    /**
+     *
+     */
+    public ContactInfoDao(Context context) {
+        helper = new MyDBOpenHelper(context);
+    }
+
+    /**
+     * 添加一条记录
+     *
+     * @param name：联系人姓名
+     * @param phone：联系人电话
+     */
+    public void add(String name, String phone) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL("insert into contactinfo (name,phone) values(?,?);", new Object[]{name, phone});
+        // 记得关闭数据库，释放资源
+        db.close();
+    }
+
+    /**
+     * 删除一条记录
+     *
+     * @param name：联系人姓名
+     */
+    public void delete(String name) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL("delete from contactinfo where name=?;", new Object[]{name});
+        // 记得关闭数据库，释放资源
+        db.close();
+    }
+
+    /**
+     * 修改联系人电话号码
+     *
+     * @param name：联系人姓名
+     * @param phone：联系人电话
+     */
+    public void update(String name, String phone) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL("update contactinfo set phone=? where name=?;", new Object[]{phone, name});
+        // 记得关闭数据库，释放资源
+        db.close();
+    }
+
+    /**
+     * 查询联系人的电话号码
+     *
+     * @param name：联系人姓名
+     */
+    public String query(String name) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select phone from contactinfo where name=?", new String[]{name});
+        String phone = null;
+        if (cursor.moveToNext()) {
+            phone = cursor.getString(0);
+        }
+        // 记得关闭数据库，释放资源
+        cursor.close();
+        db.close();
+        return phone;
+    }
+
+
 }
