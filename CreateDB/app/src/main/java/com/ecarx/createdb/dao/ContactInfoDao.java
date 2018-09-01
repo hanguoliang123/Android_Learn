8d96e29cdd082fb548192831cbf736ab9df58148
“hanguoliang123 <1073253115@qq.com>
2018-09-01 23:07:51 +0800

    03-01_创建数据库_2-20180901.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

---
+++ CreateDB/app/src/main/java/com/ecarx/createdb/dao/ContactInfoDao.java
@@ -0,0 +1,11 @@
+package com.ecarx.createdb.dao;
+
+/**
+ * @version $Rev$
+ * @authod:Guoliang.Han
+ * @des ${TODO}
+ * @updateAuthor $authod$
+ * @updateDes ${TODO}
+ */
+public class ContactInfoDao {
+}
