ba9717eb41d9be7050ab96d404b76aa9c8971470
“hanguoliang123 <1073253115@qq.com>
2018-09-16 14:00:17 +0800

    08-03_绑定服务详解-20180916.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

---
+++ BindService/app/src/main/java/com/ecarx/bindservice/IService.java
@@ -0,0 +1,14 @@
+package com.ecarx.bindservice;
+
+/**
+ * @version $Rev$
+ * @authod:Guoliang.Han
+ * @des ${TODO}
+ * @updateAuthor $authod$
+ * @updateDes ${TODO}
+ */
+public interface IService {
+    //小三提供的接口
+    public void callBanZheng(int money);
+    public void 去旅游();
+}
