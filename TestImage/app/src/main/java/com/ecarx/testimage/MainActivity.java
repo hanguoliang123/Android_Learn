cb76541ea588f6d145f6c4bb5ffbca47ca360389
“hanguoliang123 <1073253115@qq.com>
2018-10-04 23:09:16 +0800

    10-02_18分钟图像的变化_2-20181004.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- TestImage/app/src/main/java/com/ecarx/testimage/MainActivity.java
+++ TestImage/app/src/main/java/com/ecarx/testimage/MainActivity.java
@@ -16,15 +16,142 @@ public class MainActivity extends AppCompatActivity {
     private ImageView iv;
     private Bitmap bitmap;
     private float dx = 0;
+    private int degress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main2);
+        //setContentView(R.layout.activity_main2);
+        //setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main4);
         iv = (ImageView) findViewById(R.id.iv);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.meinv);
         iv.setImageBitmap(bitmap);
+    }
+
+
+
+    /**
+     * 倒影
+     * @param view
+     */
+    public void click01(View view) {
+        Matrix matrix = new Matrix();
+        matrix.setScale(1,-1);
+        matrix.postTranslate(0,bitmap.getHeight());
+
+        // 1.买一张纸
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth()*2, bitmap.getHeight()*2, bitmap.getConfig());
+
+        // 2.买个画板
+        Canvas canvas = new Canvas(newBitmap);
+
+        // 3.临摹绘画
+        Paint paint = new Paint();
+        canvas.drawColor(Color.WHITE);
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);   //消除锯齿
+
+        canvas.drawBitmap(bitmap,matrix,paint);
+        iv.setImageBitmap(newBitmap);
+    }
+
+    /**
+     * 镜面
+     * @param view
+     */
+    public void click02(View view) {
+        Matrix matrix = new Matrix();
+        matrix.setScale(-1,1);
+        matrix.postTranslate(bitmap.getWidth(),0);
+
+        // 1.买一张纸
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth()*2, bitmap.getHeight()*2, bitmap.getConfig());
+
+        // 2.买个画板
+        Canvas canvas = new Canvas(newBitmap);
+
+        // 3.临摹绘画
+        Paint paint = new Paint();
+        canvas.drawColor(Color.WHITE);
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);   //消除锯齿
+
+        canvas.drawBitmap(bitmap,matrix,paint);
+        iv.setImageBitmap(newBitmap);
+    }
+
+    /**
+     * 逆时针旋转
+     * @param view
+     */
+    public void turnCircleLeft(View view){
+        Matrix matrix = new Matrix();
+        matrix.setRotate(--degress,bitmap.getWidth(),bitmap.getHeight());
+        matrix.postTranslate(100,100);
+
+        // 1.买一张纸
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth()*2, bitmap.getHeight()*2, bitmap.getConfig());
+
+        // 2.买个画板
+        Canvas canvas = new Canvas(newBitmap);
+
+        // 3.临摹绘画
+        Paint paint = new Paint();
+        canvas.drawColor(Color.WHITE);
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);   //消除锯齿
+
+        canvas.drawBitmap(bitmap,matrix,paint);
+        iv.setImageBitmap(newBitmap);
+    }
+
+    /**
+     * 顺时针旋转
+     * @param view
+     */
+    public void turnCircleRight(View view){
+
+        new Thread(){
+            @Override
+            public void run() {
+
+                while (true) {
+
+                    Matrix matrix = new Matrix();
+                    matrix.setRotate(++degress, bitmap.getWidth(), bitmap.getHeight());
+                    matrix.postTranslate(30, 30);
+
+                    // 1.买一张纸
+                    final Bitmap newBitmap = Bitmap.createBitmap((int)(bitmap.getWidth()*1.5), (int)(bitmap.getHeight()*1.5), bitmap.getConfig());
+
+                    // 2.买个画板
+                    Canvas canvas = new Canvas(newBitmap);
+
+                    // 3.临摹绘画
+                    Paint paint = new Paint();
+                    canvas.drawColor(Color.WHITE);
+                    paint.setColor(Color.WHITE);
+                    paint.setAntiAlias(true);   //消除锯齿
+
+                    canvas.drawBitmap(bitmap, matrix, paint);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            iv.setImageBitmap(newBitmap);
+                        }
+                    });
+                    try {
+                        Thread.sleep((1));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+
+
     }
 
     /**
