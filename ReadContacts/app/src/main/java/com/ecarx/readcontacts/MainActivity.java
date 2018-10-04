821b93c14ae5363ed135b385c61f7fc09f29f971
“hanguoliang123 <1073253115@qq.com>
2018-10-04 17:11:35 +0800

    09-06_21分钟-读取联系人信息_2-20181004.
    Signed-off-by: “hanguoliang123 <“1073253115@qq.com>

--- ReadContacts/app/src/main/java/com/ecarx/readcontacts/MainActivity.java
+++ ReadContacts/app/src/main/java/com/ecarx/readcontacts/MainActivity.java
@@ -1,19 +1,38 @@
 package com.ecarx.readcontacts;
 
+import android.Manifest;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.ecarx.readcontacts.domain.ContactInfo;
+import com.ecarx.readcontacts.utils.ContactInfoUtils;
+
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+        performCodeWithPermission("读取联系人信息", new PermissionCallback() {
+            @Override
+            public void hasPermission() {
+
+            }
+
+            @Override
+            public void noPermission() {
+
+            }
+        }, Manifest.permission.READ_CONTACTS,Manifest.permission.WRITE_CONTACTS);
 
     }
 
     public void click(View view){
-        
+        List<ContactInfo> infos =  ContactInfoUtils.getAllContactInfos(this);
+        for(ContactInfo info:infos){
+            System.out.println(info.toString());
+        }
     }
 }
